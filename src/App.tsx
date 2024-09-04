75df5147a958e8aa6201d6c247008fecb21fbf57
GiovannaT <giovanna.trigueiro@gmail.com>
2024-09-04 19:58:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Botao } from './components/Botao'
+
+function App() {
+
+  return (
+    <>
+    <Botao title='teste'></Botao>
+    
+    </>
+  )
+}
+
+export default App
